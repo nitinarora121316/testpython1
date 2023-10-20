7896d2d77fc4cd7cd88798d0acd8ad72e1a63a80
Nitin Arora <nitin.arora121316@gmail.com>
2023-10-21 05:02:19 +0530

    added

---
+++ Unittest/1.py
@@ -0,0 +1,12 @@
+import unittest
+
+class a(unittest.TestCase):
+    def test_b(self):
+        print("hello")
+        print("hello")
+        print("hello")
+        print("hello")
+
+      
+if __name__ == "__main__":
+    unittest.main()
